cbf55ce0707c2428cbe651106f4c2caec6ed28aa
longhyn89 <longdn.hyn89@gmail.com>
2026-09-06 12:43:14 +0700

    Update xnhau.js

--- xnhau.js
+++ xnhau.js
@@ -12,7 +12,7 @@ function getManifest() {
         "name": "xNhau (ALL)",
         "description": "Kho clip và phim xNhau hot nhất, cập nhật liên tục.",
         "info": "Nguồn phim xNhau chất lượng cao HD/FHD.",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "baseUrl": "https://xnhau.city",
         "iconUrl": "https://raw.githubusercontent.com/hieu-TQS/movie-SuperOK/refs/heads/main/icons/xnhau.png",
         "isEnabled": true,
