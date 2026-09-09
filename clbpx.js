74087b03b923c723bf2df98fff7da5a9385fcd43
longhyn89 <longdn.hyn89@gmail.com>
2026-09-09 14:04:18 +0700

    Update clbpx.js

--- clbpx.js
+++ clbpx.js
@@ -5,7 +5,7 @@ function getManifest() {
   return JSON.stringify({
     "id": "clbpxVIP",
     "name": "CLB Phim Xưa VIP",
-    "version": "1.0.1",
+    "version": "1.0.2",
     "info": "",
     "BASEURL": BASEURL,
     "iconUrl": "https://vaxplugin.alokillgtv.workers.dev/img/clbpxVIP.png",
